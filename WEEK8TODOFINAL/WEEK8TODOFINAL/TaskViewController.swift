7a917eb95f9e557c5723dc53d5ee8499950a9c8c
Boluwatife Adesina <badesina82@gmail.com>
2021-05-19 00:43:17 +0100

    TextviewDidBegin and EndEditing

--- WEEK8TODOFINAL/WEEK8TODOFINAL/TaskViewController.swift
+++ WEEK8TODOFINAL/WEEK8TODOFINAL/TaskViewController.swift
@@ -7,11 +7,20 @@
 
 import UIKit
 
-class TaskViewController: UIViewController {
+class TaskViewController: UIViewController, UITextViewDelegate {
     var task: String?
     @IBOutlet weak var textView: UITextView!
     override func viewDidLoad() {
         super.viewDidLoad()
        textView.text = task
+        textView.delegate = self
+    }
+   
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        print("sdfgh")
+    }
+    func textViewDidEndEditing(_ textView: UITextView) {
+      print("78910")
+        textView.text = task
     }
 }
