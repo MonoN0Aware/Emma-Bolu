63930c660815a5b68d28978f355fbf7100e0358b
Emmanuel Obuh <81360481+emmanuelroyal@users.noreply.github.com>
2021-05-19 11:15:12 +0100

    Revert "Merge branch 'dev2' into EMMA-BRANCH"
    This reverts commit 72840bd666bac75e373b5dabac16e7f9a530c772, reversing
    changes made to ddda6b9f5e0b5f50acff14a0874035182b847220.

--- WEEK8TODOFINAL/WEEK8TODOFINAL/FirstViewController.swift
+++ WEEK8TODOFINAL/WEEK8TODOFINAL/FirstViewController.swift
@@ -7,53 +7,44 @@
 
 import UIKit
 
-<<<<<<< Updated upstream
-class FirstViewController: UIViewController {
-
-=======
 class FirstViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
     @IBOutlet weak var tableView: UITableView!
->>>>>>> Stashed changes
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.dataSource = self
+        tableView.delegate = self
     }
-<<<<<<< Updated upstream
-    
 
-=======
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
+func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    if let todo = todoList {
+        return todo.count
+    } else {
+        return 0
+    }
+}
+
+func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = UITableViewCell(style: .default, reuseIdentifier: nil)
+    if let todo = todoList {
+        cell.textLabel?.text =  todo[indexPath.row]
+    }
+    return cell
+}
+func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle,
+               forRowAt indexPath: IndexPath) {
+    if editingStyle == .delete {
+        todoList?.remove(at: indexPath.row)
         tableView.reloadData()
     }
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        if let todo = todoList {
-            return todo.count
-        } else {
-            return 0
-        }
-    }
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = UITableViewCell(style: .default, reuseIdentifier: nil)
-        if let todo = todoList {
-            cell.textLabel?.text =  todo[indexPath.row]
-        }
-        return cell
-    }
-    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle,
-                   forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            todoList?.remove(at: indexPath.row)
-            tableView.reloadData()
-        }
-    }
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        tableView.deselectRow(at: indexPath, animated: true)
-        guard let viewc =
-                storyboard?.instantiateViewController( withIdentifier: "task") as? TaskViewController else { return }
-        viewc.title = "New Task"
-        viewc.task = todoList?[indexPath.row]
-        viewc.solveProblem = indexPath.row
-        navigationController?.pushViewController(viewc, animated: true)
-    }
->>>>>>> Stashed changes
+}
+
+func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    tableView.deselectRow(at: indexPath, animated: true)
+    guard let viewc =
+            storyboard?.instantiateViewController( withIdentifier: "task") as? TaskViewController else { return }
+    viewc.title = "New Task"
+    viewc.task = todoList?[indexPath.row ]
+    navigationController?.pushViewController(viewc, animated: true)
+}
 }
