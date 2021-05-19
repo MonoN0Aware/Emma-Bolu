2b8968b7d081908ae7d07dc93dca126fb11b7ced
Emmanuel Obuh <81360481+emmanuelroyal@users.noreply.github.com>
2021-05-19 10:06:41 +0100

    fixed delete bug and edits now save in place

---
+++ WEEK8TODOFINAL/WEEK8TODOFINAL/constant.swift
@@ -0,0 +1,19 @@
+//
+//  File.swift
+//  WEEK8TODOFINAL
+//
+//  Created by Decagon on 5/19/21.
+//
+
+import Foundation
+var todoList: [String]?
+
+func saveData(todoList: [String]) {
+    UserDefaults.standard.set( todoList, forKey: "todoList")
+}
+
+func fetchData() -> [String]? {
+    if let todo = UserDefaults.standard.array(forKey: "todoList") as? [String] { return todo } else {
+            return nil
+    }
+}
